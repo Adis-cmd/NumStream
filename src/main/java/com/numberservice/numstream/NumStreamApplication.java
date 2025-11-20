cbab26d145f0ce4cf5b932100d2ce94ad480fbd2
Adis-cmd <priv4484@gmail.com>
2025-11-21 01:39:34 +0500

    feat: create numbers table in database

---
+++ src/main/java/com/numberservice/numstream/NumStreamApplication.java
@@ -0,0 +1,13 @@
+package com.numberservice.numstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumStreamApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/changelog/initial/001_create_table_numbers.sql
@@ -0,0 +1,6 @@
+-- changelog Adis: 001 create number table
+CREATE TABLE numbers
+(
+    id   BIGSERIAL PRIMARY KEY,
+    value int NOT NULL
+);
